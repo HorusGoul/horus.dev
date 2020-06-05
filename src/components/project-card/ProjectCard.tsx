20b85eee5c234f47f414bef36f9fd887ad4810e0
Horus Lugo <horusgoul@gmail.com>
2020-06-06 00:24:44 +0200

    feat(ProjectCard): add preview animation

--- src/components/project-card/ProjectCard.tsx
+++ src/components/project-card/ProjectCard.tsx
@@ -1,8 +1,9 @@
 import classNames from 'classnames';
 import styles from './ProjectCard.module.scss';
 import Container from '../container';
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useCallback } from 'react';
 import { RiCloseCircleLine } from 'react-icons/ri';
+import { motion, useAnimation } from 'framer-motion';
 
 export interface ProjectCardProps {
   href: string;
@@ -10,12 +11,103 @@ export interface ProjectCardProps {
   description: string;
 }
 
+const previewVariants = {
+  closed: {
+    opacity: 0,
+    pointerEvents: 'none',
+  },
+  open: {
+    opacity: 1,
+    pointerEvents: 'all',
+  },
+} as const;
+
 function ProjectCard({ href, title, description }: ProjectCardProps) {
   const [bigPreview, setBigPreview] = useState(false);
   const iframeRef = useRef<HTMLIFrameElement>(null);
+  const projectCardRef = useRef<HTMLDivElement>(null);
+  const hiddenFrameRef = useRef<HTMLDivElement>(null);
 
-  if (typeof window !== 'undefined')
-    window.addEventListener('message', (e) => console.log(e));
+  const frameControls = useAnimation();
+  const previewControls = useAnimation();
+
+  const createVariants = useCallback(() => {
+    const projectCard = projectCardRef.current;
+    const frame = hiddenFrameRef.current;
+
+    if (!projectCard || !frame) {
+      return;
+    }
+
+    const frameRect = frame.getBoundingClientRect();
+    const projectRect = projectCard.getBoundingClientRect();
+
+    const diffX = projectRect.left - frameRect.left;
+    const diffY = projectRect.top - frameRect.top;
+
+    const topPadding = 64;
+
+    const final = {
+      position: 'fixed',
+      height: document.documentElement.clientHeight - 64 - topPadding,
+      width: document.documentElement.clientWidth - 48,
+      top: 32 + topPadding,
+      left: 24,
+      transition: { type: 'tween', ease: 'easeIn', duration: 0.5 },
+    } as const;
+
+    const initial = {
+      position: 'fixed',
+      height: frame.offsetHeight,
+      width: frame.offsetWidth,
+      top: frameRect.top,
+      left: frameRect.left,
+      transition: { type: 'tween', ease: 'easeOut', duration: 0.5 },
+    } as const;
+
+    frameControls.setVariants({
+      disabled: {
+        position: 'absolute',
+        top: Math.abs(diffY),
+        left: Math.abs(diffX),
+        transition: { duration: 0 },
+      },
+      final,
+      initialFast: {
+        ...initial,
+        transition: {
+          ...initial.transition,
+          duration: 0,
+        },
+      },
+      initial,
+    });
+
+    previewControls.setVariants(previewVariants);
+
+    frameControls.start('disabled');
+  }, [frameControls, previewControls]);
+
+  const open = useCallback(async () => {
+    createVariants();
+
+    await frameControls.start('initialFast');
+    await Promise.all([
+      frameControls.start('final'),
+      previewControls.start('open'),
+    ]);
+
+    setBigPreview(true);
+  }, [frameControls, createVariants, previewControls]);
+
+  const close = useCallback(async () => {
+    await Promise.all([
+      frameControls.start('initial'),
+      previewControls.start('closed'),
+    ]);
+    await frameControls.start('disabled');
+    setBigPreview(false);
+  }, [frameControls, previewControls]);
 
   useEffect(() => {
     const iframe = iframeRef.current;
@@ -34,7 +126,7 @@ function ProjectCard({ href, title, description }: ProjectCardProps) {
       const data = e.data;
 
       if (data === 'click') {
-        setBigPreview(true);
+        open();
       }
     }
 
@@ -45,50 +137,52 @@ function ProjectCard({ href, title, description }: ProjectCardProps) {
     return () => {
       window.removeEventListener('message', listener);
     };
-  }, [href]);
+  }, [href, open]);
+
+  useEffect(() => {
+    createVariants();
+  }, [createVariants]);
 
   return (
-    <div className={classNames(styles.projectCard)}>
+    <div ref={projectCardRef} className={classNames(styles.projectCard)}>
+      <div className={styles.overlay} />
+
       <Container className={styles.content}>
-        <div>
+        <div className="relative">
           <h3 className={styles.title}>{title}</h3>
 
           <span className={styles.description}>{description}</span>
         </div>
 
-        <div className="relative">
-          <div
-            aria-hidden="true"
-            className={classNames(styles.device, 'opacity-0')}
-          >
-            <div className={styles.frame} />
-          </div>
+        <div
+          ref={hiddenFrameRef}
+          className={classNames(styles.frame, styles.fake)}
+        />
 
-          <div
-            className={classNames(styles.device, styles.real, {
-              [styles.full]: bigPreview,
-            })}
-            aria-hidden="true"
-            onClick={() => setBigPreview(false)}
-          >
-            {bigPreview && (
-              <button className={styles.close}>
-                <span>Close preview</span>
-                <RiCloseCircleLine />
-              </button>
-            )}
+        <motion.div
+          initial={previewVariants.closed}
+          animate={previewControls}
+          className={classNames(styles.device, styles.full, styles.real)}
+          onClick={close}
+        >
+          <button className={styles.close}>
+            <span>Close preview</span>
+            <RiCloseCircleLine />
+          </button>
+        </motion.div>
+
+        <motion.div animate={frameControls} className="absolute">
+          <div className="relative h-full w-full">
             <iframe
               ref={iframeRef}
               title="Atom Preview"
               src={href}
               scrolling={bigPreview ? 'yes' : 'no'}
-              className={styles.frame}
+              className={classNames(styles.frame)}
             />
           </div>
-        </div>
+        </motion.div>
       </Container>
-
-      <div className={styles.overlay} />
     </div>
   );
 }

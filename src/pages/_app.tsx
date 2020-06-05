e7657c25a564b73d41d39675c5f2771dbfc547f2
Horus Lugo <horusgoul@gmail.com>
2020-06-05 17:36:56 +0200

    feat(head): add initial-scale and minimum-scale to the meta viewport content

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import { AppProps } from 'next/app';
 import '../styles/index.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1.0,minimum-scale=1.0"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;

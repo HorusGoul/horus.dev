1cf468112ff667674a3f89324f8f1f7dfd026374
Horus Lugo <horusgoul@gmail.com>
2020-06-07 12:08:19 +0200

    feat: improve divider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app';
-import '../styles/index.css';
+import '../styles/index.scss';
 import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {

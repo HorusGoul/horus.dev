17d94c075e9834e8132003f850ff7095c02f351e
Horus Lugo <horusgoul@gmail.com>
2020-06-12 00:57:32 +0200

    feat: high quality favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,31 @@ function MyApp({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width,initial-scale=1.0,minimum-scale=1.0"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/icon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/icon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/icon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/images/icon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/images/icon/safari-pinned-tab.svg"
+          color="#000000"
+        />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#000000" />
       </Head>
       <Component {...pageProps} />
     </>

eb29a84cc1b8c391bcd9b70c68fce265f93a5de9
Horus Lugo <horusgoul@gmail.com>
2020-09-13 16:47:51 +0200

    feat(article.api.ts): add retry policy

--- src/model/article/article.api.ts
+++ src/model/article/article.api.ts
@@ -4,19 +4,28 @@ import { createArticle, Article } from './article';
 export async function fetchArticles(
   username: string,
   limit = 3,
+  retriesLeft = 5,
 ): Promise<Article[]> {
-  const response = await fetch(
-    `https://dev.to/api/articles?username=${username}&per_page=${limit}`,
-  );
-  const devArticles: DEVArticle[] = await response.json();
+  try {
+    const response = await fetch(
+      `https://dev.to/api/articles?username=${username}&per_page=${limit}`,
+    );
+    const devArticles: DEVArticle[] = await response.json();
 
-  const fullDevArticles: DEVFullArticle[] = await Promise.all(
-    devArticles.map((article) =>
-      fetch(`https://dev.to/api/articles/${article.id}`).then((result) =>
-        result.json(),
+    const fullDevArticles: DEVFullArticle[] = await Promise.all(
+      devArticles.map((article) =>
+        fetch(`https://dev.to/api/articles/${article.id}`).then((result) =>
+          result.json(),
+        ),
       ),
-    ),
-  );
+    );
 
-  return fullDevArticles.map(createArticle);
+    return fullDevArticles.map(createArticle);
+  } catch (e) {
+    if (retriesLeft > 0) {
+      return fetchArticles(username, limit, retriesLeft - 1);
+    }
+
+    throw e;
+  }
 }

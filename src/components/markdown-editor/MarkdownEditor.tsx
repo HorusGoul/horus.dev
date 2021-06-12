9ab34efc696c6516e198fde1508db544f5282f34
Horus Lugo <horusgoul@gmail.com>
2021-06-12 11:04:23 +0200

    feat: make markdown editor autoresizable

--- src/components/markdown-editor/MarkdownEditor.tsx
+++ src/components/markdown-editor/MarkdownEditor.tsx
@@ -1,5 +1,12 @@
 import { usePostEditor } from '@/contexts/post-editor';
-import React, { useState } from 'react';
+import React, {
+  forwardRef,
+  useEffect,
+  useLayoutEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
 import ReactMde from 'react-mde';
 import styles from './react-mde-styles/react-mde-all.module.scss';
 import PostRenderer from '../post-renderer';
@@ -56,8 +63,81 @@ function MarkdownEditor() {
       onTabChange={setTab}
       generateMarkdownPreview={generateMarkdownPreview}
       classes={{ reactMde: styles.markdownEditor }}
+      textAreaComponent={ResizableTextarea}
     />
   );
 }
 
 export default MarkdownEditor;
+
+const ResizableTextarea = forwardRef<
+  HTMLTextAreaElement,
+  React.TextareaHTMLAttributes<HTMLTextAreaElement>
+>(({ value, ...props }, ref) => {
+  const internalRef = useRef<HTMLTextAreaElement | null>(null);
+
+  useEffect(() => {
+    const field = internalRef.current;
+
+    function resize() {
+      if (!field) {
+        return;
+      }
+
+      field.style.height = 'auto';
+      field.style.height = field.scrollHeight + 48 + 'px';
+    }
+
+    function on(event: string, handler: () => void) {
+      field?.addEventListener(event, handler);
+    }
+
+    function off(event: string, handler: () => void) {
+      field?.removeEventListener(event, handler);
+    }
+
+    on('change', resize);
+    on('cut', resize);
+    on('paste', resize);
+    on('drop', resize);
+    on('keydown', resize);
+
+    resize();
+
+    return () => {
+      off('change', resize);
+      off('cut', resize);
+      off('paste', resize);
+      off('drop', resize);
+      off('keydown', resize);
+    };
+  }, []);
+
+  return (
+    <div
+      className="p-4 rounded-lg bg-gray-100 mb-72 transition-colors duration-200 
+    focus-within:ring-0 focus-within:bg-gray-200 outline-none font-mono"
+    >
+      <textarea
+        ref={(element) => {
+          internalRef.current = element;
+
+          if (ref === null) {
+            return;
+          }
+
+          if ('current' in ref) {
+            ref.current = element;
+          } else {
+            ref(element);
+          }
+        }}
+        value={value}
+        {...props}
+        rows={1}
+      />
+    </div>
+  );
+});
+
+ResizableTextarea.displayName = 'ResizableTextarea';

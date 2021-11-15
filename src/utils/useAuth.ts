7399c3748d0eddc83bf3969909448f887e6628ba
Horus Lugo <horusgoul@gmail.com>
2021-11-15 12:23:16 +0100

    Fix admin login issue

--- src/utils/useAuth.ts
+++ src/utils/useAuth.ts
@@ -3,12 +3,12 @@ import { Session } from '@supabase/supabase-js';
 import { useEffect, useState } from 'react';
 
 export function useAuth() {
-  const [session, setSession] = useState<Session | null | 'loading'>('loading');
+  const [session, setSession] = useState<Session | null | 'loading'>(() =>
+    supabase.auth.session(),
+  );
   const [cookie, setCookie] = useState(false);
 
   useEffect(() => {
-    setSession(supabase.auth.session());
-
     const { data } = supabase.auth.onAuthStateChange((event, session) => {
       setSession(session);
     });
@@ -23,14 +23,32 @@ export function useAuth() {
       return;
     }
 
-    fetch('/api/auth', {
-      method: 'POST',
-      headers: new Headers({ 'Content-Type': 'application/json' }),
-      credentials: 'same-origin',
-      body: JSON.stringify({ event: 'SIGNED_IN', session }),
-    })
-      .then((res) => res.json())
-      .then(() => setCookie(true));
+    let mounted = true;
+
+    async function callApiAuth() {
+      try {
+        await fetch('/api/auth', {
+          method: 'POST',
+          headers: new Headers({ 'Content-Type': 'application/json' }),
+          credentials: 'same-origin',
+          body: JSON.stringify({ event: 'SIGNED_IN', session }),
+        }).then((res) => res.json());
+
+        if (!mounted) {
+          return;
+        }
+
+        setCookie(true);
+      } catch {
+        return;
+      }
+    }
+
+    callApiAuth();
+
+    return () => {
+      mounted = false;
+    };
   }, [session]);
 
   return [session, cookie];
